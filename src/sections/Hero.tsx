03fa06fcfa3ceadc2af3553f876f63aacc9f95c5
dikiogres <dikiogres@gmail.com>
2023-03-01 14:55:30 +0700

    delete unused files

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,5 +1,3 @@
-import ButtonLink from '@/components/ButtonLink'
-import Counter from '@/components/Counter'
 import Link from 'next/link'
 import React from 'react'
 
@@ -13,14 +11,6 @@ const Hero = () => {
       <p className='text-xl font-normal text-gray-200'>
         React Data Query
       </p>
-      <div className='flex flex-wrap w-full items-center justify-center gap-3'>
-        <ButtonLink link='get-started' title='Get Started'/>
-        <ButtonLink link='basic-axios' title='Basic Axios'/>
-        <ButtonLink link='axios-tmdb' title='Axios TMDb API'/>
-        <ButtonLink link='basic-react-query' title='Basic React Query'/>
-        <ButtonLink link='pagination-react-query' title='Pagination React Query'/>
-        <ButtonLink link='rq-tmdb' title='React Query TMDb API'/>
-      </div>
       <div>
         <Link href='/movies'>
         <button className='bg-red-600 p-3 rounded-lg shadow-lg transition duration-500 hover:scale-105 hover:shadow-2xl hover:bg-red-700'>
