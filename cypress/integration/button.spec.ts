c4fbb63bdc01608869c7465d78d5d5fff2cbbc42
dikiogres <dikiogres@gmail.com>
2023-03-04 13:11:56 +0700

    chore: cypress

---
+++ cypress/integration/button.spec.ts
@@ -0,0 +1,11 @@
+// describe('Button', () => {
+//     beforeEach(() => {
+//       cy.visit('/');
+//     });
+  
+//     it('should click the button', () => {
+//       cy.findByRole('button', { name: 'Click me' }).click();
+//     });
+//   });
+  
+  export {};
